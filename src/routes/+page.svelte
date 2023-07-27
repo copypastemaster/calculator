6c3aa831eb4210c6322782f133430f045aebecd0
master <enoch.binas@gmail.com>
2023-07-27 18:16:39 +0800

    all working

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,6 +17,7 @@
    const handleInput = (e): number => {
     defaultValues.push(e.target.textContent);
     val = +(defaultValues.join(''));
+    console.log(`defaultValues = ${defaultValues} val = ${val}`)
     return val;
    }
 
@@ -35,10 +36,12 @@
         case '*':
             operator = '*';
             checker();
+            clear();
             break;
         case '/':
             operator = '/';
             checker();
+            clear();
             break;
      }
    }
@@ -54,8 +57,14 @@
    }
 
    const clear = () => {
+    defaultValues.length = 0;
+    val = 0;
+   }
+
+   const del = () => {
     val = 0;
     defaultValues.length = 0;
+    firstNum = null;
    }
 
    async function sendData (firstNumber: number | null, secondNumber: number | null, operatorer: string) {
@@ -92,20 +101,63 @@
         <button on:click={handleOperator}>+</button>
         <button on:click={handleOperator}>-</button>
         <button on:click={handleOperator}>*</button>
+        <button on:click={handleOperator}>/</button>
     </div>
     <div class="numbers">
+        <button on:click={handleInput}>7</button>
+        <button on:click={handleInput}>8</button>
+        <button on:click={handleInput}>9</button>
+        <button on:click={handleInput}>4</button>
+        <button on:click={handleInput}>5</button>
+        <button on:click={handleInput}>6</button>
         <button on:click={handleInput}>1</button>
         <button on:click={handleInput}>2</button>
         <button on:click={handleInput}>3</button>
-        <button on:click={clear}>C</button>
+        <button on:click={handleInput}>0</button>
+        <button on:click={del}>C</button>
         <button on:click={handleEqual}>=</button>
 
     </div>
 </main>
 
 <style>
+    main {
+        width: 100%;
+        max-width: 400px;
+        display: flex;
+        flex-direction: column;
+        margin: 0 auto;
+        padding: 1rem;
+        border: 1px solid;
+        border-radius: 10px;
+        box-shadow: 0 0 10px;
+        gap: 10px;
+    }
+
+    input {
+        font-size: 2rem;
+    }
+
+    .operators {
+        display: flex;
+    }
+
+    .operators button {
+        flex-grow: 1;
+    }
+
     .operators button, .numbers button {
         font-size: 2rem;
+    }
+
+    .numbers {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 20px;
+    }
+
+    .numbers button {
+        width: 120px;
     }
 </style>
 
